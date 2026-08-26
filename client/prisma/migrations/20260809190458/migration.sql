2afdd623438f3d5d6ab3ad79549fac8cf3f63a51
Ranskyth <ga.lima2205@gmail.com>
2026-08-26 16:07:11 -0300

    Refactor database schema: Remove old migrations, update table structures, and add new authentication and payment features
    - Deleted old migration files for restaurant, category, product, and user authentication tables.
    - Renamed "Restourant" to "Restaurant" and updated related foreign keys.
    - Added createdAt timestamps to Category and Product tables.
    - Introduced new authentication tables: accounts, sessions, users, and verificationtokens.
    - Added UserFavoriteRestaurant and OrderProduct tables for user interactions and order management.
    - Implemented Payment table with foreign key relationships to Order.
    - Updated Order table to include payment method and status.
    - Adjusted foreign key constraints and indexes for improved data integrity.

--- client/prisma/migrations/20260809190458/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "_CategoryToRestaurant" ADD CONSTRAINT "_CategoryToRestaurant_AB_pkey" PRIMARY KEY ("A", "B");
-
--- DropIndex
-DROP INDEX "_CategoryToRestaurant_AB_unique";
--- client/prisma/migrations/20260816025204/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `PaymentMetodo` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Order" DROP COLUMN "PaymentMetodo",
-ADD COLUMN     "paymentMetodo" TEXT;
