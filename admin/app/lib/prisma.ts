1e6b11ba1e18da3677fa6ebf14fee4d91dbb80e9
Ranskyth <ga.lima2205@gmail.com>
2026-08-26 16:06:22 -0300

    fix: update database adapter to use MySQL and adjust environment variables

--- admin/app/lib/prisma.ts
+++ admin/app/lib/prisma.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from '@/app/generated/prisma/client';
-
+import {PrismaMariaDb} from "@prisma/adapter-mariadb"
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapterMaria = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+})
 
-export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const adapterPg = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter: adapterMaria });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
